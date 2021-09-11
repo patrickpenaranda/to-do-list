ff74b86518eeb3b76f02d3c46d71fbfd72f7a9d8
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2021-09-11 11:54:00 +0800

    Added Web API

--- main.js
+++ main.js
@@ -2,13 +2,17 @@ window.addEventListener("load", () => {
 var add_btn = document.getElementById("add");
 var column_2 = document.getElementById("column-2-id");
 var column_1 = document.getElementById("column-1-id");
-var tbox = document.getElementById("textbox");
 var error_message = document.createElement("h2");
 var message_div = document.createElement("div");
 
-tbox.addEventListener("keypress", delete_error_msg, false)
 
-function delete_error_msg(key) {
+
+document.getElementById("textbox").addEventListener("click", delete_error_msg, false)
+document.getElementById("textbox").addEventListener("keypress", delete_error_msg, false)
+
+display();
+
+function delete_error_msg() {
     error_message.parentElement.remove();
 }
 
@@ -23,7 +27,7 @@ function enter_Event(key) {
     }
 }
 
-//Delete icon
+//Add btton
 add_btn.addEventListener("click", () => {
     main();
 })
@@ -31,8 +35,8 @@ add_btn.addEventListener("click", () => {
 //main function
 function main() {
     
-    var textBox = document.getElementById("textbox");
-    if(textBox.value == "") {
+    var textBox = document.getElementById("textbox").value;
+    if(textBox == "") {
         error_message.style.color = 'white';
         error_message.style.background = 'red';
         error_message.innerText = "Empty Field";
@@ -43,14 +47,8 @@ function main() {
         error_message.style.marginBottom = "0";
         message_div.appendChild(error_message);
         column_1.appendChild(message_div);
-        console.log("Empty Field!");
-        tbox.addEventListener('click', ()=> {
-            error_message.parentElement.remove();
-        })
-        
+        console.log("Empty Field!");    
     } else {
-        console.log(textBox.value);
-    
         var ul_div = document.createElement("div");
         var div_lists = document.createElement("div");
         var u_lists = document.createElement("ul");
@@ -58,62 +56,150 @@ function main() {
         var dlete_btn = document.createElement("i");
         var success = document.createElement("i");
         var dlete_btn_div = document.createElement("div");
+        var users_name = document.getElementById("two").value;
+        sessionStorage.setItem(users_name, textBox);   
 
-        success.setAttribute("class","far fa-calendar-check")
-        success.style.padding = ".5rem 20%";
-        success.style.cursor = "pointer";
-        success.style.background = "green";
-        success.style.color = "white";
+            success.setAttribute("class","far fa-calendar-check")
+            success.style.padding = ".5rem 20%";
+            success.style.cursor = "pointer";
+            success.style.background = "green";
+            success.style.color = "white";
 
-        dlete_btn.setAttribute("class", "fas fa-trash")
-        dlete_btn.style.padding = ".5rem 20%";
-        dlete_btn.style.cursor = "pointer";
-        dlete_btn.style.color = "white";
-        dlete_btn.style.background = "red";
+            dlete_btn.setAttribute("class", "fas fa-trash")
+            dlete_btn.style.padding = ".5rem 20%";
+            dlete_btn.style.cursor = "pointer";
+            dlete_btn.style.color = "white";
+            dlete_btn.style.background = "red";
 
-        lists.innerText = textBox.value;
-        lists.style.fontFamily = "Coronetscript, cursive";
-        lists.style.overflowWrap = "break-word";
-        lists.style.wordBreak = "break-word";
-        lists.style.listStyle = "none";
+            lists.innerText = textBox;
+            lists.style.fontFamily = "Coronetscript, cursive";
+            lists.style.overflowWrap = "break-word";
+            lists.style.wordBreak = "break-word";
+            lists.style.listStyle = "none";
 
-        u_lists.style.display = "flex";
-        u_lists.style.flexWrap = "wrap";
-        u_lists.append(lists);
+            u_lists.style.display = "flex";
+            u_lists.style.flexWrap = "wrap";
+            u_lists.append(lists);
 
-        ul_div.style.display = "flex";
-        ul_div.style.flexWrap = "wrap";
-        ul_div.style.width = "75%";
-        ul_div.style.maxWidth = "75%";
-        ul_div.appendChild(u_lists);
+            ul_div.style.display = "flex";
+            ul_div.style.flexWrap = "wrap";
+            ul_div.style.width = "75%";
+            ul_div.style.maxWidth = "75%";
+            ul_div.appendChild(u_lists);
 
-        div_lists.style.margin = ".5rem 0";
-        div_lists.style.display = "flex";
-        div_lists.style.borderRadius = "10px";
-        div_lists.style.border = "1px solid black";
-        div_lists.style.fontWeight = "700";
+            div_lists.style.margin = ".5rem 0";
+            div_lists.style.display = "flex";
+            div_lists.style.borderRadius = "10px";
+            div_lists.style.border = "1px solid black";
+            div_lists.style.fontWeight = "700";
 
-        dlete_btn_div.style.display = "flex";
-        dlete_btn_div.style.width = "25%";
-        dlete_btn_div.style.justifyContent = "space-between"
-        dlete_btn_div.style.padding = ".5rem .5rem";
-        dlete_btn_div.style.flexDirection = "row";
-        dlete_btn_div.appendChild(success);
-        dlete_btn_div.appendChild(dlete_btn);
-        div_lists.appendChild(ul_div);
-        div_lists.appendChild(dlete_btn_div);
+            dlete_btn_div.style.display = "flex";
+            dlete_btn_div.style.width = "25%";
+            dlete_btn_div.style.justifyContent = "space-between"
+            dlete_btn_div.style.padding = ".5rem .5rem";
+            dlete_btn_div.style.flexDirection = "row";
+            dlete_btn_div.appendChild(success);
+            dlete_btn_div.appendChild(dlete_btn);
+            div_lists.appendChild(ul_div);
+            div_lists.appendChild(dlete_btn_div);
+
+            dlete_btn.addEventListener("click", ()=> {
+                dlete_btn.parentElement.parentElement.remove();
+            })
+            column_2.append(div_lists);
+
+        }
         
-        
-        dlete_btn.addEventListener("click", ()=> {
-            dlete_btn.parentElement.parentElement.remove();
-        })
-        column_2.append(div_lists);
-        textBox.value = "";
+        document.getElementById("textbox").value = "";
+        document.getElementById("two").value = "";
     }
-}
-})
+
 
     
+
+}, false)
+
+
+function display() {
+
+
+    for(let i = 0; i < sessionStorage.length; i++) { 
+        var dlete_btn = document.createElement("i");
+        var column_2 = document.getElementById("column-2-id");
+        var textBox = document.getElementById("textbox");
+        var ul_div = document.createElement("div");
+        var div_lists = document.createElement("div");
+        var u_lists = document.createElement("ul");
+        var lists = document.createElement("li");
+        
+        var success = document.createElement("i");
+        var dlete_btn_div = document.createElement("div");
+
+            success.setAttribute("class","far fa-calendar-check")
+            success.style.padding = ".5rem 20%";
+            success.style.cursor = "pointer";
+            success.style.background = "green";
+            success.style.color = "white";
+
+            
+
+            dlete_btn.setAttribute("class", "fas fa-trash","id",`${i}`)
+            dlete_btn.style.padding = ".5rem 20%";
+            dlete_btn.style.cursor = "pointer";
+            dlete_btn.style.color = "white";
+            dlete_btn.style.background = "red";
+
+            lists.innerText = sessionStorage.getItem(sessionStorage.key(i));
+            lists.style.fontFamily = "Coronetscript, cursive";
+            lists.style.overflowWrap = "break-word";
+            lists.style.wordBreak = "break-word";
+            lists.style.listStyle = "none";
+
+            u_lists.style.display = "flex";
+            u_lists.style.flexWrap = "wrap";
+            u_lists.append(lists);
+
+            ul_div.style.display = "flex";
+            ul_div.style.flexWrap = "wrap";
+            ul_div.style.width = "75%";
+            ul_div.style.maxWidth = "75%";
+            ul_div.appendChild(u_lists);
+
+            div_lists.style.margin = ".5rem 0";
+            div_lists.style.display = "flex";
+            div_lists.style.borderRadius = "10px";
+            div_lists.style.border = "1px solid black";
+            div_lists.style.fontWeight = "700";
+
+            dlete_btn_div.style.display = "flex";
+            dlete_btn_div.style.width = "25%";
+            dlete_btn_div.style.justifyContent = "space-between"
+            dlete_btn_div.style.padding = ".5rem .5rem";
+            dlete_btn_div.style.flexDirection = "row";
+
+            dlete_btn.addEventListener("click", (event)=> {
+                const btn = event.target;
+
+                if(btn.className == "fas fa-trash") {
+                    btn.parentElement.parentElement.remove();
+                }
+            })
+
+            dlete_btn_div.appendChild(success);
+            dlete_btn_div.appendChild(dlete_btn);
+
+            
+
+            div_lists.appendChild(ul_div);
+            div_lists.appendChild(dlete_btn_div);
+
+            
+            column_2.append(div_lists);
+        }
+        
+        
+        
+    }
 
 
 
